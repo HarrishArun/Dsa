8c7d7c08c245008b50fe30304a623d39f1dd8576
HarrishArun <95520465+HarrishArun@users.noreply.github.com>
2024-01-30 19:59:20 +0530

    Create DC:150. Evaluate Reverse Polish Notation.cpp

---
+++ DC:150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+     stack<int>st;
+     auto issign=[](string token){
+         return(token=="+"|| token=="-"|| token=="*"|| token=="/");
+     };
+     for(auto token:tokens){
+         if(issign(token)){
+             int num2=st.top();
+             st.pop();
+             int num1=st.top();
+             st.pop();
+             if(token=="+"){
+                 st.push(num1+num2);
+             }
+             else if(token=="-"){
+                 st.push(num1-num2);
+             }
+             else if(token=="*"){
+                 st.push(num1*num2);
+             }
+              else{
+                 st.push(num1/num2);
+             }
+         }
+         else{ int num=stoi(token);
+         st.push(num);}
+     }   return st.top();
+    }
+};
